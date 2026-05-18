a6a043fa55e5634d9938cc87aab9a416c01aa9ae
NicolasAguilar-prog <naguilarurgiles@gmail.com>
2026-05-18 08:51:01 -0500

    UPD:METODO

--- src/App.java
+++ src/App.java
@@ -1,15 +1,43 @@
 import models.Persona;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        Persona[] personas = generatePersonas();
+        BusquedaBinaria busquedaBinaria = new BusquedaBinaria();
+        busquedaBinaria.sortByEdad(personas);
+
+
+        System.out.print("Ingrese la edad a buscar: ");
+        int edad = scanner.nextInt();
+
+
+        int edadBuscada = busquedaBinaria.findByEdad(personas, edad);
+        if (edadBuscada != -1) {
+            System.out.println("Persona encontrada en el índice: " + edadBuscada);
+        } else {
+            System.out.println("Persona no encontrada.");
+        }
+
     }
-    public static Persona[] generatePersonas(){
-        Persona[] personas = new Persona[5];
-        personas[0] = new Persona("Juan", 25);
-        personas[1] = new Persona("Maria", 30);
-        personas[2] = new Persona("Pedro", 20);
-        personas[3] = new Persona("Ana", 35);
-        personas[4] = new Persona("Luis", 28);
-        return personas;
+
+    public static Persona[] generatePersonas() {
+    Scanner scanner = new Scanner(System.in);
+    Persona[] personas = new Persona[9];
+
+    for (int i = 0; i < personas.length; i++) {
+
+        System.out.println("Ingrese Persona:");
+
+        System.out.print("Nombre: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Edad: ");
+        int edad = scanner.nextInt();
+        scanner.nextLine();
+
+        personas[i] = new Persona(nombre, edad);
     }
+    return personas;
+}
 }
--- src/BusquedaBinaria.java
+++ src/BusquedaBinaria.java
@@ -1,3 +1,37 @@
+import models.Persona;
+
 public class BusquedaBinaria {
-    
+    public void sortByEdad(Persona[] personas) {
+        for (int i = 0; i < personas.length - 1; i++) {
+            for (int j = 0; j < personas.length - i - 1; j++) {
+                if (personas[j].getEdad() > personas[j + 1].getEdad()) {
+                    Persona temp = personas[j];
+                    personas[j] = personas[j + 1];
+                    personas[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public int findByEdad(Persona[] personas, int edad){
+        int left = 0;
+        int right = personas.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            for (Persona persona : personas) {
+                System.out.print(" | " + persona.getEdad());
+            }
+            System.out.println("\n left: " + left + ", mid: " + mid + ", right: " + right + ", edad en mid: " + personas[mid].getEdad());
+            if (personas[mid].getEdad() == edad) {
+                return mid; 
+            } else if (personas[mid].getEdad() < edad) {
+                left = mid + 1; 
+            } else {
+                right = mid - 1; 
+            }
+        }
+        return -1;
+    }
 }
